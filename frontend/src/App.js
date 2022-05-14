df9706825c84c67556e68e41f8d22f4c54f62746
lizm97 <lizmizrahi.lm@gmail.com>
2022-05-14 20:28:35 +0300

    change logo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
 import logo from './logo.png';
 import './App.css';
+import React, {useState} from "react";
 
 function App() {
-  return (
-<h1>hello</h1>
-  );
+    const [] = useState ([])
+    return (
+        <>
+            <input type="text"/>
+            <button>Add cost</button>
+            <button>Clear cost</button>
+            <div>0 costs in the memory</div>
+        </>
+    );
 }
 
 export default App;
