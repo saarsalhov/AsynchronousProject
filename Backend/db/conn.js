7cec6e63b1f26fe00355baadf5a4c2b1c46429cd
lizm97 <lizmizrahi.lm@gmail.com>
2022-06-22 20:08:36 +0300

    fix codr design

--- Backend/db/conn.js
+++ Backend/db/conn.js
@@ -1,55 +1,16 @@
-const { MongoClient } = require("mongodb");
-const dotenv = require('dotenv')
-dotenv.config({ path: './config.env' });
+const {MongoClient} = require('mongodb');
+const dotenv = require('dotenv');
+dotenv.config({path: './config.env'});
 
 const connectionString = process.env.ATLAS_URI;
 
 
-
 module.exports = {
-  connectToServer:async function () {
-    const client = new MongoClient(connectionString);
-    client.connect(function(err){
-      if(err) throw err
-    })
-    return client;
-  },
+    connectToServer: async function () {
+        const client = new MongoClient(connectionString);
+        client.connect(function (err) {
+            if (err) throw err;
+        })
+        return client;
+    },
 };
-
-
-
-
-
-
-
-// async function listDatabases(client){
-//   databasesList = await client.db().admin().listDatabases();
-
-//   console.log("Databases:");
-//   databasesList.databases.forEach(db => console.log(` - ${db.name}`));
-// };
-
-
-// async function main(){
-//   /**
-//    * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
-//    * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
-//    */
-
-//   const client = new MongoClient(connectionString);
-
-//   try {
-//       // Connect to the MongoDB cluster
-//       await client.connect();
-
-//       // Make the appropriate DB calls
-//       await listDatabases(client);
-
-//   } catch (e) {
-//       console.error(e);
-//   } finally {
-//       await client.close();
-//   }
-// }
-
-// main().catch(console.error);
