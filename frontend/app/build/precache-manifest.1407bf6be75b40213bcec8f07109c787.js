7f5743bea9c629fccd597ddaba9b7a82acdaaf23
Saar Salhov <86562519+saarsalhov@users.noreply.github.com>
2022-06-23 21:21:36 +0300

    add build

---
+++ frontend/app/build/precache-manifest.1407bf6be75b40213bcec8f07109c787.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ec30ccfb3378c45fce4",
+    "url": "/static/css/main.5bd1009e.chunk.css"
+  },
+  {
+    "revision": "2ec30ccfb3378c45fce4",
+    "url": "/static/js/main.2ec30ccf.chunk.js"
+  },
+  {
+    "revision": "b9e13d1706a44556f642",
+    "url": "/static/js/1.b9e13d17.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d80abaded087c11dc69f415a8b78ad89",
+    "url": "/static/media/saar.d80abade.jpg"
+  },
+  {
+    "revision": "8bf4c6453125a18ccfdc092c7afa7c40",
+    "url": "/index.html"
+  }
+];
